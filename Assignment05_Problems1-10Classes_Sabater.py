78a83ca7ae1aefd43c2a41ca957a235d4a0a32f0
Pablo Sabater <pablosabater@Pablos-MBP-2.Home>
2024-03-07 13:02:50 -0300

    Added the first 8 actions.

--- Assignment05_Problems1-10Classes_Sabater.py
+++ Assignment05_Problems1-10Classes_Sabater.py
@@ -81,6 +81,51 @@ def main():
                                 9. Get the exponent of a number
                                 10. Find the type of an argument
                                 11. Exit. """))
-        
+        if  action == 1:
+            #Create variables for the users first and last name and input it into the greeter method
+            firstName = input("What is your first name? ")
+            lastName = input("What is your last name? ")
+            operation.greeter(firstName, lastName)
+        elif action == 2:
+            #Ask user for first and second numbers
+            num1 = int(input("Input the first number: "))
+            num2 = int(input("Input the second number: "))
+            #Print the two numbers and the sum imbeded into a string
+            print(f"\nThe sum of {num1} and {num2} is {operation.sum(num1, num2)}")
+        elif action == 3:
+            #Ask user for first and second numbers
+            num1 = int(input("Input the first number: "))
+            num2 = int(input("Input the second number: "))
+            op = input("Input the operation to be performed (+, -, *, /): ")
+            #Print the two numbers and the sum imbeded into a string
+            print(f"\nThe answer is {operation.operator(num1, num2, op)}.")
+        elif action == 4:
+            #Ask user for first number
+            num1 = int(input("Input the number: "))
+            #Print the square
+            print(f"\nThe square is {operation.Square(num1)}.")
+        elif action == 5:
+            #Ask user for first number
+            num1 = int(input("Input n: "))
+            #Print the square
+            print(f"\nThe factorial of {num1} is {operation.factorial(num1)}.")
+        elif action == 6:
+            #Ask user for first number
+            A = int(input("Input the start: "))
+            B = int(input("Input the end: "))
+            #Print the square
+            print(f"\nThe numbers from {A} to {B} are {operation.nums(A, B)}.")
+        elif action == 7:
+            #Ask user for base and height numbers
+            base = int(input("Input the base: "))
+            height = int(input("Input the height: "))
+            #Print the hypotenuse
+            print(f"\nThe hypotenuse for triangle with base {base} and height {height} is {operation.hypotenuse(base, height)}.")
+        elif action == 8:
+            #Ask user for width and height numbers
+            width = int(input("Input the base: "))
+            height = int(input("Input the height: "))
+            #Print the area
+            print(f"\nThe area for rectangle with width {width} and height {height} is {operation.areaRec(width, height)}.")
     
 
